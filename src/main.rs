d0c6347392a32eb586913067909486db590d4456
Pankaj Nikam <pankaj-nikam@users.noreply.github.com>
2022-01-31 08:32:26 +0530

    Newline characters.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
 fn main() {
     println!("{}, {}", "Hello", "World");
-    print!("First Line\nSecond Line\nThird Line");
+    println!("First Line\nSecond Line\nThird Line");
+    //All content below is displayed as new line. If we skip the \ (Backslash) then the text will be displayed as is with all the spaces and newline characters.
+    println!(
+        "{}",
+        "This \
+    is \
+    just \
+    one \
+    line."
+    );
 }
